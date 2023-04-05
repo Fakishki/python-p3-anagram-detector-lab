d942824fda6efc2a8b5cd65c75557bd96c3d1c08
Wally Pfingsten <wallypfingsten@gmail.com>
2023-04-05 05:14:23 -0700

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,24 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, test_words):
+        anagrams = []
+        for test_word in test_words:
+            lower_test_word = test_word.lower()
+            if sorted(lower_test_word) == self.sorted_word:
+                anagrams.append(test_word)
+        return anagrams
+    
+
+
+#! Unnecessary to pass -- Version below tests whether the submitted word matches word in submitted potential anagram list
+
+    # def match(self, test_words):
+    #     anagrams = []
+    #     for test_word in test_words:
+    #         lower_test_word = test_word.lower()
+    #         if lower_test_word != self.word and sorted(lower_test_word) == self.sorted_word:
+    #             anagrams.append(test_word)
+    #     return anagrams
